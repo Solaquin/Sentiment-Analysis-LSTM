07fed96320776ef3ee5b35867ee2bcc84b315276
Solaquin <145218692+Solaquin@users.noreply.github.com>
2025-10-15 22:30:45 -0500

    create vocab and advance in training module

--- training_module.py
+++ training_module.py
@@ -7,5 +7,22 @@ train = pd.read_csv("dataset_clean/train_clean.csv")
 val = pd.read_csv("dataset_clean/validation_clean.csv")
 test = pd.read_csv("dataset_clean/test_clean.csv")
 
-tok = TokenizerModule(vocab_size=20000, sequence_length=200)
-tok.fit_vectorizer(train['review_body'].astype(str))
+train_texts = train['review_body'].astype(str)
+
+tok = TokenizerModule()
+tok.fit_vectorizer(train_texts)
+tok.save_vectorizer("vectorizer")
+
+#Vectorizar los textos
+X_train = tok.vectorize_texts(train['review_body'].astype(str))
+X_val = tok.vectorize_texts(val['review_body'].astype(str))
+X_test = tok.vectorize_texts(test['review_body'].astype(str))
+
+#Tomar labels
+Y_train = train["label"]
+Y_val = val["label"]
+Y_test = test["label"]
+
+
+
+
